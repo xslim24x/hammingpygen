7dcd5ec52ad282cb661ecd23057169fdc9b15cc3
Slim Babay <slim@slimbabay.com>
2016-10-26 20:51:25 -0400

    Update hammingcode.py
    standardized variables to literature values k->n & d->k

--- hammingcode.py
+++ hammingcode.py
@@ -2,45 +2,45 @@ from optparse import OptionParser
 import math
 
 parser = OptionParser()
-parser.add_option("-l", "--length", dest="codelen", default="7",
+parser.add_option("-l", "--length", dest="codelen", default="6 ",
                   help="custom length of code")
 parser.add_option("-o", "--output", dest="outputfile", metavar="FILE",
                   help="Output codes to file")
 
 #TODO: Add parity bit function
-'''
+
 parser.add_option("-p", "--parity", dest="parity" ,
                   action="store_true", default=False,
                   help="checkbits include general parity check")
-'''
+
 parser.add_option("-e", "--e", dest="encoded" ,
-                  action="store_true", default=False,
+                  action="store_true", default=True,
                   help="display values in encoded form rather than standard form")
 
 (options, args) = parser.parse_args()
 
 # code len
-k = int(options.codelen)
+n = int(options.codelen)
 
 if options.parity:
-    k -= 1
+    n -= 1
 
 # check bits m
-m = int(math.floor(math.log(k,2)))+1
+m = int(math.floor(math.log(n,2)))+1
 
 # data bits
-d = k - m
+k = n - m
 dmax = (2**m)-m-1
 # parity
 
 
-print "Hamming Code [%d,%d]" % (k+1 if options.parity else k,d)
-print "\t total length [k] = %d" % k,
+print "Hamming Code [%d,%d]" % (n+1 if options.parity else n,k)
+print "\t total length [n] = %d" % n,
 print "+1" if options.parity else ""
-print "\t data bits [d] = %d" % d
+print "\t data bits [k] = %d" % k
 print "\t check bits [m] = %d" % m,
 print "+1" if options.parity else ""
-print "\t total codes = %d" % 2**d
+print "\t total codes = %d" % 2**k
 
 s1 = []
 s2 = []
@@ -53,7 +53,7 @@ for p in xrange(0,m):
     zeros = 2**(p)-1
     for pad in range(0,zeros):
         row += "0"
-    for i in range(1,k+1-zeros):
+    for i in range(1,n+1-zeros):
         if ((i-1)/2**(p))%2==0:
             row += "1"
         else:
@@ -67,14 +67,14 @@ for p in xrange(0,m):
 standardform = []
 if not options.encoded:
     print "\nStandard form"
-    print "D".center(d-1,' '),
+    print "D".center(k-1,' '),
     print "|",
     print "P".center(m-1,' ')
 
 for r in range(0,m):
     tempdata = ""
     temppar = ""
-    for c in range(0,k):
+    for c in range(0,n):
         if (math.log(c+1,2)%1!=0):
             tempdata += encodedform[r][c]
         else:
@@ -86,27 +86,27 @@ for r in range(0,m):
 g = []
 temp = 0
 ident = "1"
-ident += "0" * (d-1)
+ident += "0" * (k-1)
 
 print "\nG matrix:"
-for r in range(0,k):
-    if ((math.log(r+1,2)%1!=0) and options.encoded) or (not options.encoded) and r < d:
+for r in range(0,n):
+    if ((math.log(r+1,2)%1!=0) and options.encoded) or (not options.encoded) and r < k:
         g.append(ident)
         print ident
-        ident = str(bin(int(ident,base=2)>>1))[2:].zfill(d)
+        ident = str(bin(int(ident,base=2)>>1))[2:].zfill(k)
     else:
-        print standardform[temp][0:d]
-        g.append(standardform[temp][0:d])
+        print standardform[temp][0:k]
+        g.append(standardform[temp][0:k])
         temp += 1
 
 print "\nCode Words:"
 
-for c in range(0,2**d):
+for c in range(0,2**k):
     codeword = ""
     for b in g:
         weight = str.count(str(bin(int(b,base=2) & c)[2:]),"1")
         codeword += str(weight%2)
     if not options.encoded:
-        codeword = codeword[:d] + " " + codeword[d:]
+        codeword = codeword[:k] + " " + codeword[k:]
     print codeword + "\t",
     print str.count(codeword,"1")
